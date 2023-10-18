6cedb8f9c3a5c633acc9a154a91b5754175d4c64
amelie314 <qangela314@gmail.com>
2023-10-19 03:04:17 +0800

    - 加入滑鼠移動的測試 - 在 index.html 添加必要的div

--- js/map.js
+++ js/map.js
@@ -1,30 +1,52 @@
+/** @format */
+
 document.addEventListener("DOMContentLoaded", function () {
-  // 选择地图上的所有<path>元素
+  // 選擇地圖上的所有<path>元素
   let pathElements = document.querySelectorAll("path[data-name]");
 
-  // 为每个<path>元素添加点击事件监听器
+  // 創建 tooltip 元素的引用
+  var tooltip = document.querySelector(".tooltip");
+
+  // 為每個<path>元素添加點擊事件監聽器
   pathElements.forEach(function (path) {
     path.addEventListener("click", function (event) {
       updateWeatherElements();
       updateWeather36HElements();
       getAstronomicalData();
-      // 首先，从所有<path>元素中移除 'is-active' 类
+      console.log("地圖被點擊");
+      // 首先，從所有<path>元素中移除 'is-active' 類
       pathElements.forEach(function (innerPath) {
         innerPath.classList.remove("is-active");
       });
 
-      // 为被点击的<path>元素添加 'is-active' 类
+      // 為被點擊的<path>元素添加 'is-active' 類
       let currentPath = event.currentTarget;
       currentPath.classList.add("is-active");
 
-      // 获取被点击<path>元素的data-name属性值
+      // 獲取被點擊<path>元素的 data-name 屬性值
       let countyName = currentPath.getAttribute("data-name");
 
-      // 查找页面上的显示县市名称的元素，并更新其内容
+      // 查找頁面上的顯示縣市名稱的元素，並更新其內容
       let countyElement = document.getElementById("county");
       if (countyElement) {
         countyElement.textContent = countyName;
       }
+    });
+
+    // 添加滑鼠移動事件監聽器
+    path.addEventListener("mousemove", function (e) {
+      tooltip.textContent = path.getAttribute("data-name");
+      tooltip.style.left = `${e.pageX + 8}px`;
+      tooltip.style.top = `${e.pageY - 35}px`;
+      tooltip.style.display = "block";
+      console.log("滑鼠移入");
+    });
+
+    // 添加滑鼠移出事件監聽器
+    path.addEventListener("mouseout", function (e) {
+      tooltip.textContent = "";
+      tooltip.style.display = "none";
+      console.log("滑鼠移入");
     });
   });
 });
