2bed0fc39d32e054122d851a946331ebfdd03926
steward379 <steward379@gmail.com>
2023-10-19 20:50:16 +0800

    pokemon update

--- js/pokemon.js
+++ js/pokemon.js
@@ -20,25 +20,38 @@ function changePokemon(cityName, pokemon){
     const pokemonWelcome = document.querySelector("#pokemon-welcome");
 
     const bodyWrapper = document.querySelector("body div"); 
+    const footer = document.querySelector(".footer");
 
     const weatherText = document.querySelector("#Wx").textContent;
 
     console.log(pokemonWelcome, bodyWrapper, weatherText);
 
     let pokemonKing = "";
+
     if (weatherText.includes("晴天")) {
       pokemon.src = `https://static.tumblr.com/6d5fad2410cb092840ad58aa00051d01/ozvqtky/N7Onxn9yb/tumblr_static_1orkl5uzm4n40w444k88gssso.gif`
       pokemonKing = "小火龍";
-      bodyWrapper.style.backgroundColor = "rgba(255, 10, 10, 0.5)";
-      bodyWrapper.style.transition = "background-color 1s";
+      bodyWrapper.style.backgroundColor = "rgba(255, 10, 10, 0.05)";
+      bodyWrapper.style.transition = "background-color 0.7s";
+      footer.style.backgroundColor = "rgba(255, 10, 10, 0.5)";
+      footer.style.transition = "background-color 1s";
     //   pokemon.a.href="https://pokemonrevolution.net/forum/topic/54823-i-miss-my-old-pokemon/";
     // }else if (weatherText.includes("雨天")) {
 
-    }else if (weatherText.includes("多雲")) {
+    }else if (weatherText.includes("多雲") || weatherText.includes("陰天")) {
         pokemon.src = `https://media.tenor.com/Pwn9ZYb7C2QAAAAi/gengar-pokemon.gif`;
         pokemonKing = "梗鬼";
-        bodyWrapper.style.backgroundColor = "rgba(128, 0, 128, 0.5)";
-        bodyWrapper.style.transition = "background-color 0.5s";
+        bodyWrapper.style.backgroundColor = "rgba(128, 10, 128, 0.05)";
+        bodyWrapper.style.transition = "background-color 0.7s";
+        footer.style.backgroundColor = "rgba(128, 10, 128, 0.5)";
+        footer.style.transition = "background-color 0.5s";
+    }else if (weatherText.includes("雨")) {
+        pokemon.src = `https://64.media.tumblr.com/tumblr_ma4ft6OXxw1rfjowdo1_500.gif`;
+        pokemonKing = "傑尼龜";
+        bodyWrapper.style.backgroundColor = "rgba(173, 216, 230, 0.5)";
+        bodyWrapper.style.transition = "background-color 0.7s";
+        footer.style.backgroundColor = "rgba(173, 216, 230, 0.5)";
+        footer.style.transition = "background-color 0.5s";
     };
 
     pokemonWelcome.textContent = `我們是來自 ${cityName} 的團隊 ！ 去吧，${pokemonKing}! `;
