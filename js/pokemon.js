c1ba8d076610d9865a77fe250ac3f778a3c234f5
steward379 <steward379@gmail.com>
2023-10-20 00:06:47 +0800

    discord bots

--- js/pokemon.js
+++ js/pokemon.js
@@ -23,6 +23,9 @@ function changePokemon(cityName, pokemon){
 
     const footer = document.querySelector(".pokemon-footer")
 
+    const aqiContainer = document.querySelector(".aqi-container");
+    
+    console.log("這個", aqiContainer);
 
     const weatherText = document.querySelector("#Wx").textContent;
 
@@ -34,27 +37,32 @@ function changePokemon(cityName, pokemon){
       pokemon.src = `https://static.tumblr.com/6d5fad2410cb092840ad58aa00051d01/ozvqtky/N7Onxn9yb/tumblr_static_1orkl5uzm4n40w444k88gssso.gif`
       pokemon.style.display = "flex";
       pokemonKing = "小火龍";
-      bodyWrapper.style.backgroundColor = "rgba(255, 10, 10, 0.05)";
+      bodyWrapper.style.backgroundColor = "rgba(255, 10, 10, 0.03)";
+      aqiContainer.style.backgroundColor = "rgba(255, 10, 10, 0.03)";
+      aqiContainer.style.transition = "background-color 0.7s";
       bodyWrapper.style.transition = "background-color 0.7s";
       footer.style.backgroundColor = "rgba(255, 10, 10, 0.5)";
       footer.style.transition = "background-color 1s";
-    //   pokemon.a.href="https://pokemonrevolution.net/forum/topic/54823-i-miss-my-old-pokemon/";
-    // }else if (weatherText.includes("雨天")) {
-
+        //   pokemon.a.href="https://pokemonrevolution.net/forum/topic/54823-i-miss-my-old-pokemon/";
+        // }else if (weatherText.includes("雨天")) {
     }else if (weatherText.includes("多雲") || weatherText.includes("陰天")) {
         pokemon.src = `https://media.tenor.com/Pwn9ZYb7C2QAAAAi/gengar-pokemon.gif`;
         pokemon.style.display = "flex";
         pokemonKing = "梗鬼";
-        bodyWrapper.style.backgroundColor = "rgba(128, 10, 128, 0.05)";
+        bodyWrapper.style.backgroundColor = "rgba(128, 10, 128, 0.03)";
         bodyWrapper.style.transition = "background-color 0.7s";
+        aqiContainer.style.backgroundColor = "rgba(128, 10, 128, 0.03)";
+        aqiContainer.style.transition = "background-color 0.7s";
         footer.style.backgroundColor = "rgba(128, 10, 128, 0.5)";
         footer.style.transition = "background-color 0.5s ";
     }else if (weatherText.includes("雨")) {
         pokemon.src = `https://64.media.tumblr.com/tumblr_ma4ft6OXxw1rfjowdo1_500.gif`;
         pokemon.style.display = "flex";
         pokemonKing = "傑尼龜";
-        bodyWrapper.style.backgroundColor = "rgba(173, 216, 230, 0.5)";
+        bodyWrapper.style.backgroundColor = "rgba(173, 216, 230, 0.03)";
         bodyWrapper.style.transition = "background-color 0.7s";
+        aqiContainer.style.backgroundColor = "rgba(173, 216, 230, 0.03)";
+        aqiContainer.style.transition = "background-color 0.7s";
         footer.style.backgroundColor = "rgba(173, 216, 230, 0.5)";
         footer.style.transition = "background-color 0.5s";
     };
