92b6c71c68dd5ffcc4c3614acc2d5753c479038c
steward379 <steward379@gmail.com>
2023-10-20 06:18:30 +0800

    make it more stylish in short time.

--- js/pokemon.js
+++ js/pokemon.js
@@ -37,10 +37,8 @@ function changePokemon(cityName, pokemon){
       pokemon.src = `https://static.tumblr.com/6d5fad2410cb092840ad58aa00051d01/ozvqtky/N7Onxn9yb/tumblr_static_1orkl5uzm4n40w444k88gssso.gif`
       pokemon.style.display = "flex";
       pokemonKing = "小火龍";
-      bodyWrapper.style.backgroundColor = "rgba(255, 10, 10, 0.03)";
-      aqiContainer.style.backgroundColor = "rgba(255, 10, 10, 0.03)";
-      aqiContainer.style.transition = "background-color 0.7s";
-      bodyWrapper.style.transition = "background-color 0.7s";
+      document.body.style.backgroundColor = "rgba(255, 10, 10, 0.03)";
+      document.body.style.transition = "background-color 1s";
       footer.style.backgroundColor = "rgba(255, 10, 10, 0.5)";
       footer.style.transition = "background-color 1s";
         //   pokemon.a.href="https://pokemonrevolution.net/forum/topic/54823-i-miss-my-old-pokemon/";
@@ -49,20 +47,16 @@ function changePokemon(cityName, pokemon){
         pokemon.src = `https://media.tenor.com/Pwn9ZYb7C2QAAAAi/gengar-pokemon.gif`;
         pokemon.style.display = "flex";
         pokemonKing = "梗鬼";
-        bodyWrapper.style.backgroundColor = "rgba(128, 10, 128, 0.03)";
-        bodyWrapper.style.transition = "background-color 0.7s";
-        aqiContainer.style.backgroundColor = "rgba(128, 10, 128, 0.03)";
-        aqiContainer.style.transition = "background-color 0.7s";
+        document.body.style.backgroundColor = "rgba(128, 10, 128, 0.03)";
+        document.body.style.transition = "background-color 1s";
         footer.style.backgroundColor = "rgba(128, 10, 128, 0.5)";
         footer.style.transition = "background-color 0.5s ";
     }else if (weatherText.includes("雨")) {
         pokemon.src = `https://64.media.tumblr.com/tumblr_ma4ft6OXxw1rfjowdo1_500.gif`;
         pokemon.style.display = "flex";
         pokemonKing = "傑尼龜";
-        bodyWrapper.style.backgroundColor = "rgba(173, 216, 230, 0.03)";
-        bodyWrapper.style.transition = "background-color 0.7s";
-        aqiContainer.style.backgroundColor = "rgba(173, 216, 230, 0.03)";
-        aqiContainer.style.transition = "background-color 0.7s";
+        document.body.style.backgroundColor = "rgba(173, 216, 230, 0.03)";
+        document.body.style.transition = "background-color 1s";
         footer.style.backgroundColor = "rgba(173, 216, 230, 0.5)";
         footer.style.transition = "background-color 0.5s";
     };
